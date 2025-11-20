e565844392c53fc32da955ee96e7cfb276cf9c9b
ZahwaAnritia <anritia15@gmail.com>
2025-11-20 17:52:52 +0700

    column padding & spacing

--- app/src/main/java/com/example/quest6_116/view/TampilSiswa.kt
+++ app/src/main/java/com/example/quest6_116/view/TampilSiswa.kt
@@ -1,14 +1,20 @@
 package com.example.quest6_116.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.quest6_116.model.Siswa
 import com.example.quest6_116.R
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,4 +42,15 @@ fun TampilSiswa(
                 )
             )
         }
-    ){}
+    ){isiRuang ->
+        Column(
+            modifier = Modifier
+                .padding(isiRuang)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
