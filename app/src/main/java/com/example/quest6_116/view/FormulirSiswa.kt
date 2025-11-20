9bca0e507a244b4239e13fb04ff35dd02af60da1
ZahwaAnritia <anritia15@gmail.com>
2025-11-20 17:42:37 +0700

    tambah padding isiRuang pada Column

--- app/src/main/java/com/example/quest6_116/view/FormulirSiswa.kt
+++ app/src/main/java/com/example/quest6_116/view/FormulirSiswa.kt
@@ -1,9 +1,18 @@
 package com.example.quest6_116.view
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.quest6_116.R
+import com.example.quest6_116.ui.theme.Quest6_116Theme
 
 @Composable
 fun FormSiswa(
@@ -31,5 +40,10 @@ fun FormSiswa(
                 )
             )
         }
-    )
+    ){ isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
