f3b800957dcf8a0325a6dd765e03ae5c037f7f2e
ZahwaAnritia <anritia15@gmail.com>
2025-11-20 17:50:55 +0700

    tambah scaffold

--- app/src/main/java/com/example/quest6_116/view/TampilSiswa.kt
+++ app/src/main/java/com/example/quest6_116/view/TampilSiswa.kt
@@ -1,9 +1,17 @@
 package com.example.quest6_116.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.quest6_116.model.Siswa
 import com.example.quest6_116.R
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilSiswa(
     statusUISiswa: Siswa,
@@ -14,3 +22,18 @@ fun TampilSiswa(
         Pair(stringResource(id = R.string.gender), statusUISiswa.gender),
         Pair(stringResource(id = R.string.alamat), statusUISiswa.alamat)
     )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.detail),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500)
+                )
+            )
+        }
+    ){}
